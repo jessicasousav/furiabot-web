010c7a64de098e73bdf3b86cd02eff1321a621e4
jessicasousav <jessiesousavi@gmail.com>
2025-04-28 15:47:29 -0300

    update ANIMATIONS homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,23 +15,25 @@ export default function Home(){
             </div>
 
             <div className='home-content'>
-                <div className='home_botface'>
+                <div className='home-botface'>
                     <img src={botface} alt='botface'/>
                 </div>
-                <div className='content_title'>
-                    <h2>
-                        Olá, guerreiro(a)!
-                    </h2>
+                <div className='content-title'>
+                    <div className='content-hi'>
+                        <h2>
+                            Olá, guerreiro(a)!
+                        </h2>
+                    </div>
                     <h3>
-                        Bem-vindo(a) ao nosso FURIAbot, um chat dinâmico onde você pode encontrar informações sobre nosso time.
+                        Sou o FuriaBOT, um chatbot dinâmico da FURIA onde você pode encontrar informações sobre o nosso time.
                     </h3>
                 </div>
 
-                <div className='content_messages'>
+                <div className='content-messages'>
                     <h4>
                         Mas não paramos por aí, se quiser apenas jogar um pouquinho, preparamos um pequeno quiz para você testar seus conhecimentos sobre esse time tão querido pelo Brasil!
                     </h4>
-                    <Link to='/chat' className='content_link'>
+                    <Link to='/chat' className='content-link'>
                         Clique aqui e bora conversar um pouquinho!
                     </Link>
                     <p>Te espero lá viu, guerreiro?!</p>
